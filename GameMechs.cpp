c9b19323891477a85dbd06a7acf03a29eda8fca1
hasanf13 <hasanf13@mcmaster.ca>
2025-11-28 18:23:53 -0500

    F's iterations complete

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -1,12 +1,22 @@
 #include "GameMechs.h"
 
-GameMechs::GameMechs()
+GameMechs::GameMechs() // used to initialize
 {
-    
+    exitFlag = false;
+    loseFlag = false;
+    score = 0; 
+    input = 0;
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
+    // check if board size < 0
+    if(boardX < 0 || boardY < 0){
+        throw std::invalid_argument("Invalid values.");
+    }
+
+    boardSizeX = boardX;
+    boardSizeY = boardY;
     
 }
 
@@ -14,59 +24,62 @@ GameMechs::GameMechs(int boardX, int boardY)
 
 bool GameMechs::getExitFlagStatus() const
 {
-
+    return exitFlag;
 }
 
 bool GameMechs::getLoseFlagStatus() const
 {
-
+    return loseFlag;
 }
     
 
 char GameMechs::getInput() const
 {
-
+    return input;
 }
 
 int GameMechs::getScore() const
 {
-
+    return score;
 }
 
 void GameMechs::incrementScore()
 {
-    
+    score += 1;
 }
 
 int GameMechs::getBoardSizeX() const
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY() const
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
+    exitFlag = true;
 
 }
 
 void GameMechs::setLoseFlag()
 {
-    
+    loseFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    if (MacUILib_hasChar()){
+        input = MacUILib_getChar();
+    }
 }
 
 void GameMechs::clearInput()
 {
-
+    input = 0;
 }
 
 // More methods should be added here
--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -1,5 +1,77 @@
 #include "objPosArrayList.h"
 
-// Paste your Tested implementation here.
-// Paste your Tested implementation here.
-// Paste your Tested implementation here.
+objPosArrayList::objPosArrayList(){ // default constructor
+    aList = new objPos[ARRAY_MAX_CAP];
+    listSize = 0;
+
+    arrayCapacity = ARRAY_MAX_CAP;
+}
+
+objPosArrayList::~objPosArrayList(){ // destructor
+    delete[] aList; // deallocating data
+}
+
+objPos objPosArrayList::getHeadElement() const{
+    return aList[0];
+}
+
+objPos objPosArrayList::getTailElement() const{
+    return aList[listSize - 1];
+}
+
+int objPosArrayList::getSize() const{
+    return listSize;
+}
+
+objPos objPosArrayList::getElement(int index) const{
+    return aList[index];
+}
+
+void objPosArrayList::insertHead(objPos thisPos){
+    if(listSize == arrayCapacity){
+        MacUILib_printf("No space left");
+    }
+    else{
+        int i;
+        for(i = listSize - 1; i >= 0; i--){
+            aList[i+1] = aList[i]; // shifting of variables
+        } 
+        
+        aList[0] = thisPos;
+        listSize += 1;
+    }
+
+}
+
+void objPosArrayList::insertTail(objPos thisPos){
+    if(listSize == arrayCapacity){
+        MacUILib_printf("No space left");
+    }
+    else{
+        aList[listSize] = thisPos;
+        listSize++;
+    }
+
+}
+
+void objPosArrayList::removeHead(){
+    if(listSize == 0){
+        MacUILib_printf("No elements left"); // empty list
+    }
+    else{
+        int i;
+        for(i = 1; i < listSize; i++){
+            aList[i-1] = aList[i];
+        }
+        listSize--;
+    }
+}
+
+void objPosArrayList::removeTail(){
+    if(listSize == 0){ 
+        MacUILib_printf("No elements left"); // empty list
+    }
+    else{
+        listSize--; // makes last element accessible
+    }
+}
