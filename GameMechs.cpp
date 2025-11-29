d8b1538c517c2c558dfc65bdaf78bb8a791fc769
hasanf13 <hasanf13@mcmaster.ca>
2025-11-28 19:24:47 -0500

    modified GameMechs

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -1,4 +1,5 @@
 #include "GameMechs.h"
+#include <time.h>
 
 GameMechs::GameMechs() // used to initialize
 {
@@ -6,6 +7,7 @@ GameMechs::GameMechs() // used to initialize
     loseFlag = false;
     score = 0; 
     input = 0;
+    srand((unsigned int) time(NULL));
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
@@ -17,6 +19,11 @@ GameMechs::GameMechs(int boardX, int boardY)
 
     boardSizeX = boardX;
     boardSizeY = boardY;
+    exitFlag = false;
+    loseFlag = false;
+    score = 0; 
+    input = 0;
+    srand((unsigned int) time(NULL));
     
 }
 
@@ -75,6 +82,9 @@ void GameMechs::setInput(char this_input)
     if (MacUILib_hasChar()){
         input = MacUILib_getChar();
     }
+    else{
+        input = this_input;
+    }
 }
 
 void GameMechs::clearInput()
@@ -83,3 +93,38 @@ void GameMechs::clearInput()
 }
 
 // More methods should be added here
+void GameMechs::generateFood(objPosArrayList* playerPosList){
+    int xRange = boardSizeX;
+    int yRange = boardSizeY;
+    int xmin = 1, xmax = xRange - 2;
+    int ymin = 1, ymax = yRange - 2;
+
+    char foodchar = 'o';
+    int valid = 0;
+
+    while (!valid){
+        int playX = (rand() % (xmax - xmin + 1)) + xmin;
+        int playY = (rand() % (ymax - ymin + 1)) + ymin;
+        int clash = 0;
+
+       int i;
+       for (i = 0; i < playerPosList->getSize() ; i++){
+            objPos snakeseg = playerPosList->getElement(i);
+            if(playX == snakeseg.pos->x && playY == snakeseg.pos->y){ // check the positions for each segment
+                clash = 1; // collision detected
+                break; // continues looping and generating random numbersuntil valid equals 1
+            }
+       }
+
+       if (clash == 0){
+            food.setObjPos(playX, playY, foodchar); // store the food object
+            valid = 1; // exit the loop
+       }
+  
+    }
+
+}
+
+objPos GameMechs::getFoodPos() const{
+    return food;
+}
